250200bc3627cee76831e8d25a43c35282046753
Joey Dreijer <info@d3vzer0.com>
2019-01-06 21:34:07 +0100

    Removed scripts and METTA files. Migrated to reternal-quickstart repo

--- mitre/Discovery/discovery_system_time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# MITRE PHASE: DISCOVERY
-# MITRE TECHNIQUE: System Time Discovery
-
-# System Time Discovery (various)
-
-echo "Windows Command: w32tm /tz"
-vagrant winrm -c "w32tm /tz" #w32tm /tz
