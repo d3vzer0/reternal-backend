edc30e071ddbbe97c4f9bda204344605a27d4c97
Joey Dreijer <info@d3vzer0.com>
2019-01-13 04:33:42 +0100

    Changed API to view task results and ability to download raw files

--- app/api_results.py
+++ app/api_results.py
@@ -3,7 +3,7 @@ from app import app, api, celery, jwt
 from app.models import Commands, Tasks, TaskResults, BeaconHistory
 from app.operations import Task, Result
 from app.validators import Existance
-from flask import Flask, request, g
+from flask import Flask, request, g, make_response
 from flask_restful import Api, Resource, reqparse
 from mongoengine.queryset.visitor import Q
 from flask_jwt_extended import (
@@ -17,20 +17,22 @@ class APIResults(Resource):
 
     def __init__(self):
         self.args = reqparse.RequestParser()
-        if request.method == "POST":
-            self.args.add_argument('beacon_id', location='json', required=True, help='Beacon ID', type=str)
-            self.args.add_argument('task_id', location='json', required=True, help='Task ID', type=str)
         if request.method == "GET":
+            content_choices = ('image', 'text', 'other')
             self.args.add_argument('beacon_id', location='args', required=True, help='Beacon ID', type=str)
+            self.args.add_argument('content_type', location='args', required=True, help='Content Type (Magic)', choices=content_choices, type=str)
 
     def get(self):
         args = self.args.parse_args()
-        pipeline = [{"$lookup": {"from": "task_results", "localField": "_id","foreignField": "task_id", "as": "output"}}]
-        task_results = Tasks.objects(beacon_id=args.beacon_id).aggregate(*pipeline)
+        content_mapping = {
+            'image':'image/png',
+            'text':'text/plain',
+            'other':''
+        }
+        pipeline = [{'$lookup': {"from":"fs.files", "as":"file", "let":{"file_id":"$output"},
+            "pipeline":[{"$match":{"$expr":{"$and":[{"$eq":["$_id", "$$file_id"]}, {"$eq":["$contentType", content_mapping[args.content_type]]}]}}}]}},{'$unwind':'$file'}]
+        task_results = TaskResults.objects(beacon_id=args.beacon_id).aggregate(*pipeline)
         results = json.loads(loadbson(task_results))
-        # for output in task_results:
-        #     result = {"beacon_id":output.beacon_id, "task_id":output.task_id, "end_date":output.end_date}
-        #     results.append(result)
         return results
 
 
@@ -42,12 +44,10 @@ class APIAttachment(Resource):
     decorators = [jwt_required]
 
     def get(self, task_id):
-        task_result = TaskResults.objects.get(taskId=task_id)
+        task_result = TaskResults.objects.get(id=task_id)
         task_file = task_result.output.read()
-        filename = "%s-output" %(task_id)
         response = make_response(task_file)
-        response.headers['Content-Type'] = "application/octet-stream"
-        response.headers["Content-Disposition"] = "attachment; filename=%s" %(filename)
+        response.headers['Content-Type'] = task_result.output.contentType
         return response
 
     def delete(self, task_id):
@@ -55,3 +55,4 @@ class APIAttachment(Resource):
         return deleteTask
 
 api.add_resource(APIAttachment, '/api/v1/result/<string:task_id>')
+
--- app/api_tasks.py
+++ app/api_tasks.py
@@ -1,4 +1,5 @@
 import datetime, hashlib, random,json, urllib, time
+from bson.json_util import dumps as loadbson
 from app import app, api, celery, jwt
 from app.models import Tasks, TaskResults
 from app.operations import Task
@@ -16,26 +17,34 @@ class APITasks(Resource):
     decorators = [jwt_required]
 
     def __init__(self):
-        self.parser = reqparse.RequestParser()
+        self.args = reqparse.RequestParser()
         if request.method == 'POST':
             date_epoch = int(datetime.datetime.now().timestamp())
-            self.parser.add_argument('name', type=str, required=True, location='json')
-            self.parser.add_argument('commands', type=list, required=True, location='json')
-            self.parser.add_argument('beacon_id', type=str, required=True, location='json')
-            self.parser.add_argument('start_date', type=int, required=False, default=date_epoch, location='json')
+            self.args.add_argument('name', type=str, required=True, location='json')
+            self.args.add_argument('commands', type=list, required=True, location='json')
+            self.args.add_argument('beacon_id', type=str, required=True, location='json')
+            self.args.add_argument('start_date', type=int, required=False, default=date_epoch, location='json')
 
+        if request.method == "GET":
+            self.args.add_argument('beacon_id', location='args', required=True, help='Beacon ID', type=str)
+
+    def get(self):
+        args = self.args.parse_args()
+        pipeline = [{"$lookup": {"from": "task_results", "localField": "_id","foreignField": "task_id", "as": "output"}}]
+        task_results = Tasks.objects(beacon_id=args.beacon_id).aggregate(*pipeline)
+        results = json.loads(loadbson(task_results))
+        return {'count':30, 'data':results}
 
     def post(self):
-        args = self.parser.parse_args()
-        print(args.commands)
+        args = self.args.parse_args()
         verify_beacon = Existance.beacon(beacon_id=args['beacon_id'])
         if verify_beacon['result'] == "success":
             start_date = datetime.datetime.fromtimestamp(args.start_date)
             result = Task.create(args.beacon_id, args.commands, start_date, args.name)
         else:
-            result = {"tasks":[]}
+            result = verify_beacon
 
-        return "result"
+        return result
 
  
 api.add_resource(APITasks, '/api/v1/tasks')
