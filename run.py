f33d2de75037e099bfad0d9bb07316a2fecb6482
Joey Dreijer <info@d3vzer0.com>
2019-01-20 02:16:53 +0100

    Changed default port and default CORS policy

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=app.config['FLASK_PORT'])
+    socketio.run(app, debug=True, host="0.0.0.0", port=app.config['FLASK_PORT'])
