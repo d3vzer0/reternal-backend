eb38724ebddb087b3073379096b899478712b85e
Joey Dreijer <jdreijer@d3vzer0.com>
2020-04-27 21:37:49 +0200

    Added prefix for env for easier configuration

--- app/environment.py
+++ app/environment.py
@@ -3,20 +3,20 @@ import datetime
 import os
 
 config = {
-    'CELERY_BACKEND': os.getenv('CELERY_BACKEND', 'redis://localhost:6379'),
-    'CELERY_BROKER': os.getenv('CELERY_BACKEND', 'redis://localhost:6379'),
-    'MONGO_HOST': os.getenv('MONGO_HOST', 'localhost')
+    'CELERY_BACKEND': os.getenv('RT_CELERY_BACKEND', 'redis://localhost:6379'),
+    'CELERY_BROKER': os.getenv('RT_CELERY_BACKEND', 'redis://localhost:6379'),
+    'MONGO_HOST': os.getenv('RT_MONGO_HOST', 'localhost')
 }
 
 routes = {
     'agent.*': {
-        'queue': os.getenv('AGENT_QUEUE', 'agent')
+        'queue': os.getenv('RT_AGENT_QUEUE', 'agent')
     },
     'api.*': {
-        'queue': os.getenv('API_ROUTE', 'api')
+        'queue': os.getenv('RT_API_ROUTE', 'api')
     },
     'c2.*': {
-        'queue': os.getenv('C2_ROUTE', 'c2')
+        'queue': os.getenv('RT_C2_ROUTE', 'c2')
     }
 }
 
