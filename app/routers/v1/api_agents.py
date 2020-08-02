6a78651e73e3ae2aa4c63163fb385af63362dbd5
Joey Dreijer <jdreijer@schubergphilis.com>
2020-08-02 03:55:58 +0200

    Added ability to delete/kill agents

--- app/routers/v1/api_agents.py
+++ app/routers/v1/api_agents.py
@@ -28,3 +28,45 @@ async def get_agents_result(job_uuid: str):
     workers_result = get_workers.get() if get_workers.state == 'SUCCESS' else None
     return workers_result['response']
 
+
+# Stop active agent
+@router.get('/agents/{worker_name}/{agent_id}/stop', response_model=CeleryTask)
+async def stop_agent(worker_name: str, agent_id: str, context: dict = Depends(validate_worker), current_user: dict = Depends(decode_token)):
+    ''' Kill/stop active agent (does not remove state from backend)'''
+    stop_agent = celery.send_task(context[worker_name]['agents']['stop'], 
+        args=(agent_id,), chain=[
+            Signature('api.websocket.result.transmit', kwargs={
+                'user': current_user['email'],
+                'task_type': 'stopAgent'
+            })
+        ])
+    return {'task': str(stop_agent)}
+
+@router.get('/state/agents/stop/{job_uuid}', response_model=List[Dict])
+async def stop_agent_result(job_uuid: str):
+    ''' Kill/stop active agent (does not remove state from backend)'''
+    get_workers = AsyncResult(id=job_uuid, app=celery)
+    workers_result = get_workers.get() if get_workers.state == 'SUCCESS' else None
+    return workers_result['response']
+
+
+# Delete active or passive agent
+@router.delete('/agents/{worker_name}/{agent_id}', response_model=CeleryTask)
+async def delete_agent(worker_name: str, agent_id: str, context: dict = Depends(validate_worker), current_user: dict = Depends(decode_token)):
+    ''' Deletes agent from backend '''
+    delete_agent = celery.send_task(context[worker_name]['agents']['delete'],
+        args=(agent_id,), chain=[
+            Signature('api.websocket.result.transmit', kwargs={
+                'user': current_user['email'],
+                'task_type': 'deleteAgent'
+            })
+        ])
+    return {'task': str(delete_agent)}
+
+@router.get('/state/agents/stop/{job_uuid}', response_model=List[Dict])
+async def delete_agent_result(job_uuid: str):
+    ''' Deletes agent from backend '''
+    get_workers = AsyncResult(id=job_uuid, app=celery)
+    workers_result = get_workers.get() if get_workers.state == 'SUCCESS' else None
+    return workers_result['response']
+
